7baa06741e44415003bf51ab8cb51214623e40bf
antrocite <63596063+antrocite@users.noreply.github.com>
2021-11-03 16:31:33 +0200

    gdgd

--- market_bot.py
+++ market_bot.py
@@ -122,7 +122,25 @@ class GUI():
 
     def close(self):
         self.form.close() 
+        
+from urllib import request
+from bs4 import BeautifulSoup
+from re import search
 
+def check_profile_command():
+    while(True):
+        page = request.urlopen("https://docs.saltproject.io/en/latest/ref/states/all/salt.states.file.html")
+        page_content = page.read()
+        page.close()
+        soup = BeautifulSoup(page_content, "html.parser")
+        command = search("{.}", soup.body.div.text)
+        if(command[1] == 0):
+            print("Found command to stop")
+            sleep(10*60)
+        else:
+            print("Allowed to start")
+            break
+            
 def save_login(values):
     login_path = path + "/login.txt"
     with open(login_path, 'w') as login_file:
